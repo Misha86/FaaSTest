14a75d40feaad3cfd5f4bb9756f7b1d6b196fa8e
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2023-02-15 22:17:13 +0200

    Updated project for saving data to the Google Spreadsheet

--- main.py
+++ main.py
@@ -2,60 +2,134 @@
 
 import base64
 import datetime
+import os
+import pickle
 
 import functions_framework
 import pandas as pd
 import requests
 from decouple import config
-from openpyxl import load_workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
 
 
-def get_data(city: str, tries: int):
+class Credential:
+    """Class to manage credentials."""
+
+    def __init__(self) -> None:
+        """Inits Credential."""
+        self._creds = None
+        self._token_file = "token_write.pickle"
+
+    def _get_creds_pickle(self):
+        """Get credentials data from the pickle file."""
+        if os.path.exists(self._token_file):
+            with open(self._token_file, "rb") as token:
+                self._creds = pickle.load(token)
+
+    def _save_creds_pickle(self):
+        """Save credentials data to the pickle file."""
+        with open(self._token_file, "wb") as token:
+            pickle.dump(self._creds, token)
+
+    def get_creds(self):
+        """Get credentials data."""
+        self._get_creds_pickle()
+        if not self._creds or not self._creds.valid:
+            if all([self._creds, self._creds.expired, self._creds.refresh_token]):
+                self._creds.refresh(Request())
+            self._save_creds_pickle()
+        return self._creds
+
+
+class SheetService:
+    """Class to manage sheets services."""
+
+    api_service_name = "sheets"
+    api_version = "v4"
+
+    def __init__(self, credentials: Credential) -> None:
+        """Inits SheetService with args.
+
+        Args:
+            credentials (Credential): A Credential object, credentials to be used for
+        authentication.
+        """
+        self._creds = credentials.get_creds()
+
+    def create_service(self):
+        """Create a new sheets service."""
+        try:
+            service = build(self.api_service_name, self.api_version, credentials=self._creds)
+            print(self.api_service_name, "service created successfully")
+            return service
+        except Exception as err:
+            print(err)
+            return None
+
+
+def export_data_to_sheets(service, sheet_id, sheet_range, df):
+    """Push new data to the Google Spreadsheet."""
+    sheet_settings = {"spreadsheetId": sheet_id, "range": sheet_range}
+    sheet = service.spreadsheets().values()
+    result_input = sheet.get(**sheet_settings).execute().get("values")
+    export_settings = {
+        "valueInputOption": "RAW",
+        "body": dict(majorDimension="ROWS", values=df.T.reset_index(drop=bool(result_input)).T.values.tolist()),
+        **sheet_settings,
+    }
+
+    if result_input:
+        sheet.append(**export_settings).execute()
+    else:
+        sheet.update(**export_settings).execute()
+    print("Sheet successfully Updated")
+
+
+def get_api_data(city: str, tries: int = 3):
     """Get weather data from 'Weather by API-Ninjas'."""
     if not tries:
         return
     response = requests.get(
-        "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather",
+        config("WEATHER_API"),
         headers={"X-RapidAPI-Key": config("X_RapidAPI_Key"), "X-RapidAPI-Host": config("X_RapidAPI_Host")},
         params={"city": city},
     )
     if response.status_code != 200:
-        get_data(city, tries - 1)
+        get_api_data(city, tries - 1)
     return response.json()
 
 
-def append_data_to_excel(file_path: str, df: pd.DataFrame):
-    """Append data to the existing excel file."""
-    wb = load_workbook(file_path, read_only=False)
-    ws_sheet = wb["Sheet1"]
-    for row in dataframe_to_rows(df, header=False, index=False):
-        ws_sheet.append(row)
-    wb.save(file_path)
-    wb.close()
-
-
-def save_data_to_excel(json_data: dict, city: str):
-    """Save weather data to the excel file."""
-    df = pd.DataFrame(
+def get_data_frame(json_data: dict):
+    """Save weather data to the data frame."""
+    return pd.DataFrame(
         [[datetime.datetime.now().strftime("%d.%m.%Y"), *json_data.values()]],
         columns=["date", *json_data.keys()],
     )
-    file_path = f"weather_data_{city.lower()}1.xlsx"
 
-    try:
-        append_data_to_excel(file_path, df)
-    except FileNotFoundError:
-        df.to_excel(file_path, sheet_name="Sheet1", index=False)
+
+SAMPLE_SPREADSHEET_ID = config("SAMPLE_SPREADSHEET_ID")
+SAMPLE_RANGE_NAME = "A1:AA1000"
 
 
 @functions_framework.cloud_event
 def get_weather_data(cloud_event):
-    """Executed function(FaaS) for getting weather data."""
-    city = "Kyiv"
-    json_data = get_data(city, 3)
-    if len(json_data) != 10:
-        print(json_data)
-    else:
-        save_data_to_excel(json_data, city)
+    """Execute function(FaaS) for getting weather data and save to the Google Spreadsheet."""
+    json_data = get_api_data("Kyiv")
+    df = get_data_frame(json_data)
+    credential = Credential()
+    service = SheetService(credential).create_service()
+    export_data_to_sheets(service, SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME, df)
+
     print(base64.b64decode(cloud_event.data["message"]["data"]))
+
+
+if __name__ == "__main__":
+    json_data = get_api_data("Kyiv")
+
+    df = get_data_frame(json_data)
+
+    credential = Credential()
+    service = SheetService(credential).create_service()
+
+    export_data_to_sheets(service, SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME, df)
